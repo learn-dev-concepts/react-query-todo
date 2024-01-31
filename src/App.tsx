89b2fb59d73b1e7e1792dd373f852727a322c8cd
devsince2021 <dev.ryu2021@gmail.com>
2024-02-01 00:19:14 +0900

    complete todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 import { getTodos } from "./apis/todos";
+import { useQueries, useQuery } from "@tanstack/react-query";
 
 interface Todo {
-  id: number;
+  id: string;
   title: string;
+  isDone: boolean;
 }
 
 const App = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
-    getTodos().then(setTodos);
+    getTodos()
+      .then((todoArr) => todoArr.map((todo) => ({ ...todo, isDone: false })))
+      .then(setTodos);
   }, []);
 
+  // const { data } = useQuery({
+  //   queryKey: ["todo"],
+  //   queryFn: getTodos,
+  // });
+
   const handleClick = (todo: Todo) => {
-    console.log("id: >> ", todo.id);
+    const newTodo = { ...todo, isDone: !todo.isDone };
+    const newList = todos.map((t) => (t.id === newTodo.id ? newTodo : t));
+
+    setTodos(newList);
   };
 
   return (
@@ -24,10 +36,13 @@ const App = () => {
         <button
           onClick={() => handleClick(todo)}
           key={todo.id}
-          className="h-10 my-5 bg-red-100 w-56 flex flex-col"
+          className="h-10 my-5 bg-red-100 w-56 flex flex-row"
         >
           <div>{todo.id}</div>
+          <div className="px-2">/</div>
           <div>{todo.title}</div>
+          <div className="px-2">/</div>
+          <div>{todo.isDone ? "done" : "not yet"} </div>
         </button>
       ))}
     </div>
--- src/apis/todos.ts
+++ src/apis/todos.ts
@@ -2,7 +2,12 @@ import axios from "axios";
 
 const url = "http://localhost:4000/todos";
 
-export const getTodos = async () => {
+interface TodoResponse {
+  id: string;
+  title: string;
+}
+
+export const getTodos = async (): Promise<TodoResponse[]> => {
   const res = await axios(url, { method: "GET" });
   const data = res.data;
 
